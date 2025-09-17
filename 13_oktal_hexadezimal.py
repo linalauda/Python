f766829241db5a305b023c18e08b98bf284d7f7d
Alina Kudrina <linakudrina@gmail.com>
2025-09-17 13:54:23 -0400

    lesson 13

---
+++ 13_oktal_hexadezimal.py
@@ -0,0 +1,5 @@
+octal_value = 0o71
+print(octal_value)
+
+hex_value = 0x71
+print(hex_value)
---
+++ 13_simple_arithmetic.py
@@ -0,0 +1,7 @@
+my_float = 20.5
+my_int = 5
+my_int + my_float
+print(my_int + my_float)
+
+b = my_int % my_float
+print(my_int % my_float)
