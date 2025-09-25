1350e5290fe6042d673d98912980ec494abe3e4e
Alina Kudrina <linakudrina@gmail.com>
2025-09-25 15:23:33 -0400

    tag 14

---
+++ 14_Strings.py
@@ -0,0 +1,30 @@
+text = "Hallo Welt"
+print(text)
+
+print("qwertyuio")
+print('123456789qwertyuio')
+
+my_string = "Bob said, \"All your bases belong to us!\""
+print(my_string)
+
+text2 = "Zeile 1\nZeile 2 \t mit Tab"
+print(text2)
+
+multi_line_string = """Das ist ein mehrzeiliger
+String, der über mehrere
+Zeilen geht."""
+print(multi_line_string)
+
+a = "Hallo"
+b = "Welt"
+print(a + ' ' + b)
+print(a * 3)    
+print(len(a))
+print(a[0])
+print(a[1])
+print(a[-1])
+print(a[1:4])
+print(a[1:])
+print(a[:3])
+print(a[1:4:2])
+print(a[::-1])
