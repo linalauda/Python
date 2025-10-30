af7b880a6a0a6ccf9903dd8dfc44eaeffc497a45
Alina Kudrina <linakudrina@gmail.com>
2025-10-30 15:00:38 -0400

    Python Ausdruck und Zuweisung PEMDAS

---
+++ 21_Ausdruck_Zuweisung.py
@@ -0,0 +1,35 @@
+# Ausdruck (Expression) 
+
+y = 5 + 3
+print("y =", y)
+
+
+#Zuweisung (Assignment)
+
+x = 10
+print ("x =", x)
+
+# Statement - fuhrt eine Action aus x = 5, if, for, def
+
+print (y)
+
+#Einfache Zuweisung
+a = 7
+print("a =", a) 
+
+#Mehrfache Zuweisung
+b, c, d = 1, 2, 3
+print("b =", b)
+print("c =", c)
+print("d =", d)
+
+#Tausch von Werten
+e = 4
+f = 5
+print("Vor dem Tausch: e =", e, ", f =", f)
+
+#Kettenzuweisung
+g = h = i = 0
+print("g =", g)
+print("h =", h)
+print("i =", i)
